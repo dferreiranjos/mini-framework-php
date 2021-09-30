928d77928e15d6fb8a2e5796652508b14703497b
dferreiranjos <dferreiranjos@yahoo.com.br>
2021-09-29 21:47:25 -0300

    routes

--- app/config/Router.php
+++ app/config/Router.php
@@ -1,2 +1,24 @@
 <?php
 
+$this->get('/', function(){
+    echo 'Início';
+});
+
+$this->get('/home', function(){
+    echo 'Estou na HOME!';
+});
+
+
+$this->get('/about/teste', function(){
+    echo 'Estou na ABOUT!';
+});
+
+$this->get('/categoria', function(){
+    echo 'categoria';
+});
+
+
+
+
+
+
